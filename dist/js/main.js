281b15643529b2683f4d6d6092ca9928bcb6eac3
AniruddhaSoni <anirudh.soni007@gmail.com>
2021-07-29 18:53:33 +0530

    added console log in line54

--- dist/js/main.js
+++ dist/js/main.js
@@ -52,7 +52,7 @@ async function requestSearches() {
 
 search_button.addEventListener("click", () => {
   requestSearches().then((response) => {
-    console.log(response);
+    console.log("Hey anirudh");
   });
 });
 
